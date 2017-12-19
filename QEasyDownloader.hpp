d7831816a7fed0acb6d94aa666d45f10bf092c87
antony-jr <antonyjr@protonmail.com>
2017-12-19 19:44:31 +0530

    drawing the prototypes!

--- QEasyDownloader.hpp
+++ QEasyDownloader.hpp
@@ -39,10 +39,152 @@
 #if !defined(QEASY_DOWNLOADER_HPP_INCLUDED)
 #define QEASY_DOWNLOADER_HPP_INCLUDED
 #include <QtCore>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
 
 
-class QEasyDownloader {
+/*
+ * Class QEasyDownloader <- Inherits QObject
+ * --------------------
+ *
+ *  The main class that handles downloads like a pro.
+ *
+ *  Constructors:
+ *	QEasyDownloader(QObject *parent = NULL);
+ *  	QEasyDownloader(QObject *parent = NULL , QNetworkManager *toUseManager);
+ *
+ *  Methods:
+ *  	void Debug();
+ *  	void ResumeDownloads();
+ *  	void SaveState();
+ *  
+ *  Private Slots:
+ *	void download();
+ *	void finishedHead();
+ *   	void finished();
+ *   	void downloadProgress(qint64 , qint64);
+ *  Public Slots:
+ *	void Download(const QUrl& , const QString&);
+ *	void Download(const QUrl&);
+ *	void Stop();
+ *	void Resume();
+ *  Signals:
+ *	void DownloadFinished(const QString&);
+ *	void DownloadProgress(qint64 , qint64 , qint64);
+ *	void error(QNetworkReply::NetworkError);
+ *	void timeout();
+ *  Private:
+ *	QNetworkAccessManager* _pManager;
+ *	QNetworkRequest _CurrentRequest;
+ *	QNetworkReply* _pCurrentReply;	
+ *	int _nDownloadTotal,
+ *	    _nDownloadSize, 
+ *	    _nDownloadSizeAtPause;
+ *   	bool _bAcceptRanges,
+ *	     StopDownload = false,
+ *	     doResumeDownload = false,
+ *	     doSaveState = false,
+ *	     doDebug = false;
+ *   	QTimer _Timer;	
+ *
+*/
+class QEasyDownloader : public QObject {
+	Q_OBJECT
+public:
+    explicit QEasyDownloader(QObject *parent = NULL)
+	    : QObject(parent)
+    {
+	    _pManager = new QNetworkAccessManager(this);
+	    _Access   = new QMutex(this);
+    }
 
-};
+    explicit QEasyDownloader(QObject *parent = NULL , QNetworkManager *toUseManager)
+    	    : QObject(parent)
+    {
+	    _pManager = toUseManger;
+	    _Access   = new QMutex(this);
+    }
+    
+    void Debug()
+    {
+	doDebug = !doDebug;
+    }
 
+    void ResumeDownloads()
+    {
+	doResumeDownloads = !doResumeDownloads;
+    }
+
+    void SaveState()
+    {
+	doSaveState = !doSaveState;
+    }
+
+    ~QEasyDownloader()
+    {
+	    _pManager->deleteLater();
+	    _Access->deleteLater();
+    }
+
+private slots:
+    void download()
+    {
+    }
+
+    void finishedHead()
+    {
+    }
+
+    void finished()
+    {
+    }
+
+    void downloadProgress(qint64 bytesReceived , qint64 bytesTotal)
+    {
+    }
+
+public slots:
+    void Download(const QUrl& givenURL , const QString& fileName)
+    {
+    }
+    
+    void Download(const QUrl& givenURL)
+    {
+    }
+
+    void Stop()
+    {
+    }
+
+    void Resume()
+    {
+    }
+
+signals:
+    /*
+     * I'm only giving the parameters a name because it would be easy to
+     * find its meaning!
+    */
+    void DownloadFinished(const QString& fileName);
+    void DownloadProgress(qint64 bytesReceived , qint64 bytesTotal qint64 speed);
+    void error(QNetworkReply::NetworkError errorCode);
+    void timeout();
+
+private:
+    QNetworkAccessManager*    _pManager;
+    QNetworkRequest           _CurrentRequest;
+    QNetworkReply*            _pCurrentReply;
+    QMutex*		      _Access;
+    int _nDownloadTotal = 0,
+	_nDownloadSize = 0,
+	_nDownloadSizeAtPause = 0;
+    bool _bAcceptRanges = false,
+	 StopDownload = false,
+	 doResumeDownloads = false,
+	 doSaveState = false,
+	 doDebug = false;
+    QTimer _Timer;
+
+};  // Class QEasyDownloader END
 #endif // QEASY_DOWNLOADER_HPP_INCLUDED
